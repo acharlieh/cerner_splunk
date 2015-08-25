2c9995e1f5038086ac383ac5f8565ef9f77d5f6a
Charlie Huggard <charlie@huggard.info>
2015-08-25 17:04:10 -0500

    Add provides on resources and providers for Chef 13 compat

--- resources/forwarder_monitors.rb
+++ resources/forwarder_monitors.rb
@@ -7,6 +7,9 @@
 
 actions :install, :delete
 
+provides :cerner_splunk_forwarder_monitors
+provides :splunk_forwarder_monitors
+
 attribute :app,      kind_of: String, name_attribute: true, regex: [/^[A-Za-z0-9_-]/]
 attribute :index,    kind_of: String, required: false
 attribute :monitors, kind_of: Array, default: []
